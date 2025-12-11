4d7188abe8272603a9ac8544c9c1078286b84928
JadedPigeon <JadedPigeon137@gmail.com>
2025-12-11 14:20:28 -0500

    RMQ connection string

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMq: %s\n", err)
+		return
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connected to RabbitMQ successfully!")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Peril server shutting down...")
 }
