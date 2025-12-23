e0410b728a58fb1e3729eb386fc9e44e052a2c45
JadedPigeon <JadedPigeon137@gmail.com>
2025-12-23 17:09:17 -0500

    Added client logic to DeclareAndBind queues

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMq: %s\n", err)
+		return
+	}
+	defer connection.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, _, err = pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.TransientQueue)
+	if err != nil {
+		fmt.Printf("Failed to declare and bind queue: %s\n", err)
+		return
+	}
+	//defer channel.Close()
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Peril client shutting down...")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,6 +3,7 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -25,4 +26,41 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 			Body:        jsonBytes,
 		},
 	)
+}
+
+// an enum to represent "durable" or "transient"
+type SimpleQueueType string
+
+const (
+	DurableQueue   SimpleQueueType = "durable"
+	TransientQueue SimpleQueueType = "transient"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	newChannel, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Failed to open a channel in DeclareAndBind: %s\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	newQueue, err := newChannel.QueueDeclare(queueName, queueType == DurableQueue, queueType == TransientQueue, queueType == TransientQueue, false, nil)
+	if err != nil {
+		fmt.Printf("Failed to declare a queue in DeclareAndBind: %s\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	// Bind the queue to the exchange
+	err = newChannel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		fmt.Printf("Failed to bind queue to exchange in DeclareAndBind: %s\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	return newChannel, newQueue, nil
 }
